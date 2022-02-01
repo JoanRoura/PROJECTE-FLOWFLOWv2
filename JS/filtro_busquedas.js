2ca48bdf31c1c8122686633b560f6bdba438fc5e
JoanGarcia8 <joan.garcia@institutvidreres.cat>
2022-02-01 08:14:40 +0100

    Commit Joan Garcia

---
+++ JS/filtro_busquedas.js
@@ -0,0 +1,11 @@
+
+/*export default function searchFilters(input,selector){
+const d=document;
+
+d.addEventListener("keyup", e => {
+    if(e.target.matches(input)){
+    console.log(e.key);
+    }
+    
+})
+}*/
