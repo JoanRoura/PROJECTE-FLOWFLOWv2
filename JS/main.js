f4a72101dba06bdc6999a3b8ef3108ba5ce76fe1
JoanGarcia8 <joan.garcia@institutvidreres.cat>
2022-02-01 21:19:38 +0100

    Revert "Commit Joan Garcia"
    This reverts commit 2ca48bdf31c1c8122686633b560f6bdba438fc5e.

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,8 @@
-/*const d= document;
-const a=document;
-a.addEventListener("DOMContentLoaded",e=>{
-    (searchFilters(".productos-filter", ".producto"));
-    console.log(#producto)
-    });
-    function searchFilters(input,selector){
-        
-       
-    d.addEventListener("keyup", e => {
-        if(e.target.matches(input)){
-        //console.log(e.key);
-        console.log(e.target.value)
-        d.querySelectorAll(selector).forEach(el =>{el.textContent.toLowerCase().
-        includes(e.target.value)
-        ? el.classList.remove("filter")
-        :el.classList.add("filter")
-        })
-        }
-        
-    })
-    
-    
-    
-}*/
+/* window.sr = ScrollReveal();
 
-
-
+    sr.reveal('.productosAparicion', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-100px',
+    });  
+*/
